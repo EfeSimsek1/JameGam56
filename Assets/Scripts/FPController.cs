4732143adf09982bbd65ae559724cbdfdeb12e7d
EfeSimsek1 <efe.simsek.ca@gmail.com>
2026-01-20 19:13:50 -0500

    added virtual cameras

--- Assets/Scripts/FPController.cs
+++ Assets/Scripts/FPController.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class FPController : MonoBehaviour
 {
+    PlayerState currentState;
+
     [Header("Movement Parameters")]
     public float maxSpeed = 3.5f;
     public float acceleration = 15f;
@@ -31,19 +35,22 @@ public class FPController : MonoBehaviour
 
     [Header("Components")]
     [SerializeField] public CharacterController characterController;
-    [SerializeField] public Camera fpCamera;
+    [SerializeField] public GameObject fpCamera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentState = PlayerState.FreeMove;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveUpdate();
-        LookUpdate();
+        if (currentState == PlayerState.FreeMove)
+        {
+            MoveUpdate();
+            LookUpdate();
+        }
     }
 
     private void MoveUpdate()
@@ -89,4 +96,15 @@ public class FPController : MonoBehaviour
         if (characterController == null) characterController = GetComponent<CharacterController>();
     }
 
+    IEnumerator TransitionCamera()
+    {
+        yield return null;
+    }
+
+    public enum PlayerState
+    {
+        FreeMove,
+        CameraTransition,
+        InTask
+    }
 }
